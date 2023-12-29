5eb77ea7da7051e7acf8c916a6c3d562fe4cb41c
lerochkagucci <147007203+lerochkagucci@users.noreply.github.com>
2023-12-28 20:38:48 -0800

    add singleton

--- src/03_Singleton/singleton.py
+++ src/03_Singleton/singleton.py
@@ -1,2 +1,7 @@
-class Singleton:
-    pass
+class Singleton: 
+    _instance = None 
+ 
+    def __new__(cls): 
+        if cls._instance is None: 
+            cls._instance = super().__new__(cls) 
+        return cls._instance
